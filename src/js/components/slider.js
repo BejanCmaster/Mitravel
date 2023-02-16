5b5437666095aa5095c68832fa6b4b8e41c39a63
Cristian Bejan <cristian.bejan@intranet.posta.md>
2023-02-16 15:10:38 +0200

    Finish Section Feature Videos

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -1,1 +1,13 @@
+import Swiper, {
+  Navigation,
+  Pagination
+} from 'swiper';
 
+Swiper.use([Navigation, Pagination]);
+
+const swiper = new Swiper('.slider-container', {
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 105,
+  slidesPerView: 'auto'
+});

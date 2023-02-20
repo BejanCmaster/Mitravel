90c7139eb0647749a5b3d246bbf4f96977851455
Cristian Bejan <cristian.bejan@intranet.posta.md>
2023-02-20 16:50:19 +0200

    Mitravel

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -5,9 +5,52 @@ import Swiper, {
 
 Swiper.use([Navigation, Pagination]);
 
-const swiper = new Swiper('.slider-container', {
+
+const swiper1 = document.querySelector('.slider-container'),
+  swiper2 = document.querySelector('.swiper-container'),
+  playButtonsFirst = document.querySelectorAll('.main-slider__play');
+
+let swiperSlider1 = new Swiper(swiper1, {
   centeredSlides: true,
   loop: true,
   spaceBetween: 105,
   slidesPerView: 'auto'
+});
+
+let swiperSlider2 = new Swiper(swiper2, {
+  centeredSlides: true,
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 1,
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  navigation: {
+    nextEl: '.btn-left',
+    prevEl: '.btn-right',
+  },
+});
+
+swiperSlider2.on('transitionEnd', function () {
+  let videos = document.querySelectorAll('.first__slider video');
+  videos.forEach((el) => {
+    el.pause();
+    el.currentTime = 0;
+  });
+  playButtonsFirst.forEach((el) => {
+    el.style.display = 'block';
+  });
+});
+
+
+playButtonsFirst.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let video = e.currentTarget.closest('.main-slider__media').querySelector('video');
+    video.play();
+    e.currentTarget.style.display = 'none';
+    setTimeout(() => {
+      video.volume = 0.5;
+    }, 1000);
+  });
 });
